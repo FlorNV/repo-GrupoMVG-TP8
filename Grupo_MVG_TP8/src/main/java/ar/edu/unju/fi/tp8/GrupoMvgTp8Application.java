688dfc6e2aa4aff32c3a787eb88f9d940b82dd39
sofim <sofim@LAPTOP-SOFIAM>
2021-05-31 15:51:27 -0300

    Agrego anotaciones a la clase producto y modifico controller y template

--- Grupo_MVG_TP8/src/main/java/ar/edu/unju/fi/tp8/GrupoMvgTp8Application.java
+++ Grupo_MVG_TP8/src/main/java/ar/edu/unju/fi/tp8/GrupoMvgTp8Application.java
@@ -8,6 +8,10 @@ public class GrupoMvgTp8Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrupoMvgTp8Application.class, args);
+		//Integrantes del equipo:
+		//Valdez Florencia Noelí
+		//Muñoz Sofía Julieta
+		//Gallardo Matías Joaquin
 	}
 
 }
